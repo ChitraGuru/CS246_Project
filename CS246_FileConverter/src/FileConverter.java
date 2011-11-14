9d7a036bf1c8bac7ed08cc6b24394a7cfb341827
Chitra <chitra.aq@gmail.com>
2011-11-13 18:51:02 -0800

    Updating actors sequence

--- CS246_FileConverter/src/FileConverter.java
+++ CS246_FileConverter/src/FileConverter.java
@@ -1,4 +1,5 @@
 import java.io.FileReader;
+import java.util.Comparator;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class FileConverter {
 
@@ -21,6 +24,9 @@ public class FileConverter {
 		m_OutputFileName = iOutputFile;
 	}
 	
+	//This is for getting movie and rating in a single file
+	//Format:
+	//movieID	CriticsRating
 	public boolean ConvertFile()
 	{
 		boolean retVal = true;
@@ -86,10 +92,134 @@ public class FileConverter {
 		catch( FileNotFoundException e)
 		{
 			System.out.println("source file not found");
+			retVal = false;
 		}
 		catch( Exception e)
 		{
 			System.out.println("Error");
+			retVal = false;
+		}
+		
+		
+		
+		return retVal;
+	}
+	
+	public boolean ConvertActorsFile( String iActorsList )
+	{
+		boolean retVal = true;
+		
+		try
+		{
+			
+			//First process the list of actors and create an array of all the actors
+		/*	FileInputStream actorsFile = new FileInputStream( iActorsList );
+			DataInputStream inActorsDataStream= new DataInputStream(actorsFile);
+			BufferedReader bufferedInActorsDataStream = new BufferedReader(new InputStreamReader(inActorsDataStream));
+			
+			String[] actorsList = new String[96000];
+			String actorName;
+			int i = 0;
+			String oldActorName = " ";
+			while ((actorName = bufferedInActorsDataStream.readLine()) != null)   
+			{			
+				if( 0 != actorName.compareTo(oldActorName) )
+				{
+					actorsList[i++] = actorName;
+					oldActorName = actorName;
+				}
+			}
+			
+			*/
+			
+			//Do the actual reading of the actors list now
+			FileInputStream sourceFile;
+
+			//Check if input file exists
+			sourceFile = new FileInputStream(m_InputFileName);
+						
+			
+			//Open input file
+			DataInputStream inDataStream= new DataInputStream(sourceFile);
+			BufferedReader bufferedInDataStream = new BufferedReader(new InputStreamReader(inDataStream));
+			
+			//Output file writer
+			BufferedWriter bufferedOutDataStream = new BufferedWriter(new FileWriter(m_OutputFileName));
+		    
+			
+			//Read the file line by line
+			String strLine;
+			boolean nextLine = true;
+			int oldMovieID = 1;
+			int actorIndex = 0;
+			String strToWrite = "";
+			HashMap hashMap = new HashMap();
+			int maxActors = -1;
+			while ((strLine = bufferedInDataStream.readLine()) != null )   
+			{
+				//Split the line by the delimiter
+				String[] result = strLine.split("\\t");
+			
+				//0 is ID
+				//1 is actor ID
+				//2 is	actor Name
+				//3 is ranking
+				int movieID = Integer.parseInt(result[0]);
+				String actorID = result[1];
+				int actorRanking = Integer.parseInt(result[3]);
+				
+				//Are we starting a new movie here?
+				if( oldMovieID != movieID )
+				{
+
+					//First process the data
+					Iterator iterator = hashMap.keySet().iterator();
+				       
+				       
+			        while(iterator. hasNext()  )
+			        {        
+
+			            strToWrite = strToWrite.concat(Integer.toString(oldMovieID));
+			            strToWrite = strToWrite.concat(",");
+			            
+			            Object key = iterator.next();
+			            strToWrite = strToWrite.concat((String) key);
+			            strToWrite = strToWrite.concat(",");
+			            
+			            int actorRank = (maxActors +1 -(Integer)(hashMap.get(key)));
+			            double NormalizedRank = (double)actorRank/(double)maxActors;
+			            		
+			            strToWrite = strToWrite.concat(Double.toString(NormalizedRank));
+
+			            strToWrite = strToWrite.concat("\n");
+			        }
+					
+					hashMap.clear();
+					System.out.println(strToWrite);
+					bufferedOutDataStream.write(strToWrite);
+				    nextLine = true;
+				    actorIndex = 0;
+				    strToWrite = "";
+				    oldMovieID = movieID;
+				}
+				hashMap.put(actorID, actorRanking);
+				maxActors = maxActors>actorRanking?maxActors:actorRanking;
+			}
+			 if (bufferedOutDataStream != null) 
+			 {
+				 bufferedOutDataStream.flush();
+				 bufferedOutDataStream.close();
+             }
+		}
+		catch( FileNotFoundException e)
+		{
+			System.out.println("source file not found");
+			retVal = false;
+		}
+		catch( Exception e)
+		{
+			System.out.println("Error");
+			retVal = false;
 		}
 		
 		
--- CS246_FileConverter/src/Main.java
+++ CS246_FileConverter/src/Main.java
@@ -7,8 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		FileConverter fileConverter = new FileConverter("..\\Files\\Initial\\movies.dat","..\\Files\\Movie_Critic_Rating.txt");
-		fileConverter.ConvertFile();
+	//	FileConverter fileConverter = new FileConverter("..\\Files\\Initial\\movies.dat","..\\Files\\Movie_Critic_Rating.txt");
+		//fileConverter.ConvertFile();
+		
+		FileConverter fileConverterForActor = new FileConverter("..\\Files\\Initial\\movie_actors.dat","..\\Files\\Movie_Actor_Ranking.txt");
+		fileConverterForActor.ConvertActorsFile("..\\Files\\Initial\\actors_list.txt");
 	}
 
 }
